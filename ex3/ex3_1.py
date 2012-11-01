add5caca34bd1bea5a0d4a675487115c89cdee23
yk <yannic.kilcher@gmail.com>
2012-11-01 01:30:09 +0100

    done

--- ex3/ex3_1.py
+++ ex3/ex3_1.py
@@ -1,9 +1,14 @@
-from numpy import *
-from numpy.core.numeric import asarray, identity, ndarray, zeros
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from numpy import max, min, float64, sum
+from numpy.core.numeric import asarray, identity, ndarray, zeros, count_nonzero, ones
 from numpy.ma.extras import dot
-from pypr.clustering.gmm import mulnormpdf, logmulnormpdf
+from numpy.oldnumeric.rng import random_sample
+from pypr.clustering.gmm import mulnormpdf, logmulnormpdf, gauss_ellipse_2d, gm_assign_to_cluster
 from scipy.cluster.vq import kmeans2
 from scipy.io.matlab.mio import loadmat
+import numpy
+import pylab
+#from numpy import *
 
 #class Gaussian:
 #    def __init__(self, mean, covm):
@@ -21,40 +26,65 @@ class GaussianMM:
         self.c = asarray([1.0 / self.N] * self.N)
     
     def getP(self,sample):
-        p = ndarray((sample.shape[0]))
+        p = zeros(sample.shape[0])
         for k in range(0,self.N):
             p += self.c[k]*mulnormpdf(sample, self.mean[k], self.covm[k])
-        return p
+        return p.reshape((-1,1))
     
-    def getLogP(self,sample):
-        p = zeros((sample.shape[0]))
-        for k in range(0,self.N):
-            p += self.c[k]*logmulnormpdf(sample, self.mean[k], self.covm[k])
-        return p
+    def draw(self,data):
+        if self.dim == 2:
+            fig = pylab.figure()
+            #ax = Axes(fig)
+            ass = gm_assign_to_cluster(data,self.mean,self.covm,self.c)
+            clrs = 'brgy'
+            for i in range(self.N):
+                pts = data[ass == i]
+                #pts = pts[::100]
+                pylab.plot(pts[:,0],pts[:,1],'.',c=clrs[i])
+                x1, x2 = gauss_ellipse_2d(self.mean[i], self.covm[i])
+                pylab.plot(x1, x2, 'k', linewidth=2)
+            pylab.show()
+        elif self.dim == 3:
+            fig = pylab.figure()
+            ax = Axes3D(fig)
+            ass = gm_assign_to_cluster(data,self.mean,self.covm,self.c)
+            clrs = 'brgy'
+            for i in range(self.N):
+                pts = data[ass == i].astype(float64)
+                pts = pts[::100]
+                ax.scatter(pts[:,0],pts[:,1],pts[:,2],c=clrs[i])
+            pylab.show()
 
-def gmmEM(data, K, it):
-    centroid = kmeans2(data, K)[0]
+def gmmEM(data, K, it,show=False,usekmeans=True):
+    #data += finfo(float128).eps*100
+    centroid = kmeans2(data, K)[0] if usekmeans else ((max(data) - min(data))*random_sample((K,data.shape[1])) + min(data))
     N = data.shape[0]
     gmm = GaussianMM(centroid)
+    if show: gmm.draw(data)
     while it > 0:
         it = it - 1
         # e-step
-        gausses = ndarray((K, N), dtype='float64')
+        gausses = zeros((K, N), dtype = data.dtype)
         for k in range(0, K):
             gausses[k] = gmm.c[k]*mulnormpdf(data, gmm.mean[k], gmm.covm[k])
         sums = sum(gausses, axis=0)
+        if count_nonzero(sums) != sums.size:
+            raise "Divide by Zero"
         gausses /= sums
         # m step
         sg = sum(gausses, axis=1)
+        if count_nonzero(sg) != sg.size:
+            raise "Divide by Zero"
         gmm.c = ones(sg.shape) / N * sg
         for k in range(0, K):
             gmm.mean[k] = sum(data * gausses[k].reshape((-1,1)), axis=0) / sg[k]
             d = data - gmm.mean[k]
             d1 = d.transpose()*gausses[k]
             gmm.covm[k]=dot(d1,d)/sg[k]
+        if show: gmm.draw(data)
     return gmm
 
 if __name__ == '__main__':
-    data = loadmat('data/skin.mat')['sdata'].reshape((-1, 3))
-    gmm = gmmEM(data,3,1)
+    data = loadmat('data/gmmdata.mat')['gmmdata'].reshape((-1, 2))
+    gmm = gmmEM(data,3,10,True,False)
     #em(data,3,max_iter=2)
--- ex3/ex3_2.py
+++ ex3/ex3_2.py
@@ -1,28 +1,61 @@
 from ex3_1 import gmmEM
-from numpy.core.numeric import seterr
+from numpy.core.numeric import seterr, float128, float64, count_nonzero
 from numpy.lib.shape_base import dstack
+from numpy.ma.core import logical_not, logical_and
+from pypr.clustering.gmm import em
 from scipy.io.matlab.mio import loadmat
 from scipy.misc.pilutil import imread
-import pylab
-from pypr.clustering.gmm import em
 import numpy
+import pylab
+
+class GroundTruth:
+    mask = None
+    notMask = None
+    positives = None
+    negatives = None
+    
+    def __init__(self,fileName):
+        self.mask = imread(fileName) > 0
+        self.notMask = logical_not(self.mask)
+        self.positives = count_nonzero(self.mask)*1.0
+        self.negatives = (self.mask.size - self.positives)*1.0
+        
+    def checkClassification(self,binary):
+        fp = count_nonzero(logical_and(self.notMask, binary))
+        tp = count_nonzero(logical_and(self.mask,binary))
+        #falsePosRate = fp/self.negatives
+        #falsePosRate = fp/(eps+count_nonzero(binary))
+        #truePosRate = tp/self.positives
+        #truePosRate = tp/(eps+count_nonzero(logical_not(binary)))
+        #return falsePosRate, truePosRate
+        return fp,tp
 
 def getBlackWhiteFromBinary(img):
     return dstack((img,img,img))
 
 if __name__ == '__main__':
     seterr(all='warn')
-    skindata, nonskindata = loadmat('data/skin.mat')['sdata'].reshape((-1, 3)).astype(float), loadmat('data/nonskin.mat')['ndata'].reshape((-1, 3)).astype(float)
-    gmmskin, gmmnonskin = gmmEM(skindata, 2, 5), gmmEM(nonskindata, 2, 5)
+    skindata, nonskindata = loadmat('data/skin.mat')['sdata'].reshape((-1, 3)).astype(float128), loadmat('data/nonskin.mat')['ndata'].reshape((-1, 3)).astype(float128)
+    iters = 10
+    show = False
+    usekmeans = False
+    gmmskin, gmmnonskin = gmmEM(skindata, 2, iters,show,usekmeans), gmmEM(nonskindata, 2, iters,show,usekmeans)
     #g1,g2 = em(skindata,2,max_iter=1),em(nonskindata,2,max_iter=1)
-    img = imread('data/image.png').astype(float) / 255.0
+    img = imread('data/image.png').astype(float128) / 255.0
     imshape = img.shape
     img = img.reshape((-1, 3))
     #for sample in img:
         #gmmskin.getLogP(sample)
-    skinp, nonskinp = gmmskin.getLogP(img), gmmnonskin.getLogP(img)
+    skinp, nonskinp = gmmskin.getP(img), gmmnonskin.getP(img)
     res = (skinp > nonskinp)
     res = res.reshape((imshape[0],imshape[1]))
+    res = logical_not(res)
+    
+    gt = GroundTruth('data/mask.png')
+    fp, tp = gt.checkClassification(res)
+    print "false positive ratio: ", fp*1.0/count_nonzero(logical_not(res))
+    print "true positive ratio: ", tp*1.0/count_nonzero(res)
+    
     res = getBlackWhiteFromBinary(res)
     pylab.figure()
     pylab.imshow(res)
