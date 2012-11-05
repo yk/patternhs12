095ef0ac268e429f3d51bb6005d0de6804e2494f
yk <yannic.kilcher@gmail.com>
2012-11-05 18:10:22 +0100

    dummyclassifier works

--- ex4/classifiers.py
+++ ex4/classifiers.py
@@ -1,5 +1,7 @@
+from numpy import float64
+from numpy.core.numeric import ndarray, count_nonzero
 def calculateMisclassificationRate(ours, real):
-    return 1.0 - count_nonzero(ours + real).astype(float64) / ours.size
+    return 1.0 - count_nonzero(ours + real)*1.0 / ours.size
 
 class LinearClassifier:
     def train(self, data, labels):
@@ -8,16 +10,25 @@ class LinearClassifier:
     
     def test(self, data, labels):
         ourLabels = self.classify(data)
-        return calculateMisclassificationRate(ourLabels, labels)
+        return calculateMisclassificationRate(ourLabels.flatten(), labels)
     
     def classify(self, data):
         '''input: data to classify, output: array of labels (1/-1)'''
         pass
     
+class DummyClassifier(LinearClassifier):
+    def classify(self, data):
+        labels = ndarray((data.shape[0],1),dtype=bool)
+        labels.fill(True)
+        return labels
+        
+    def train(self, data, labels):
+        return 0
+    
 class LogisticRegressionClassifier(LinearClassifier):
     def classify(self, data):
         pass
     
-    def test(self, data, labels):
+    def train(self, data, labels):
         pass
     
--- ex4/ex4.py
+++ ex4/ex4.py
@@ -1,21 +1,23 @@
+from classifiers import DummyClassifier
 from numpy import float64
 from numpy.core.fromnumeric import transpose
 from numpy.core.numeric import count_nonzero
-from scipy.io.matlab.mio import loadmat
 from numpy.lib.shape_base import vsplit
+from scipy.io.matlab.mio import loadmat
 
 
 def doTest(dataSetName, classifier):
     allData = loadmat('data/' + dataSetName + '.mat')
     trainLblsAndData = transpose(allData[dataSetName+'_train'])
     testLblsAndData = transpose(allData[dataSetName+'_test'])
-    trainLabels, trainData = vsplit(trainLblsAndData, 1)
-    testLabels,testData = vsplit(testLblsAndData, 1)
+    trainLabels, trainData = trainLblsAndData[:,0],trainLblsAndData[:,1:]
+    testLabels,testData = testLblsAndData[:,0],testLblsAndData[:,1:]
     classifier.train(trainData,trainLabels)
     mcR = classifier.test(testData,testLabels)
     return mcR
     
 
 if __name__=='__main__':
-    myClassifier = LinearClassifier()
+    myClassifier = DummyClassifier()
     micRate1 = doTest('toy',myClassifier)
+    print micRate1
