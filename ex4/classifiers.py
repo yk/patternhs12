b1f71eb65f992aa9d8b3ba52a580b24708566130
yk <yannic.kilcher@gmail.com>
2012-11-05 17:03:45 +0100

    rearrange

--- ex4/classifiers.py
+++ ex4/classifiers.py
@@ -1,4 +1,19 @@
-from ex4 import *
+def calculateMisclassificationRate(ours, real):
+    return 1.0 - count_nonzero(ours + real).astype(float64) / ours.size
+
+class LinearClassifier:
+    def train(self, data, labels):
+        '''input: training data and corresponding labels(1,-1), output: nothing'''
+        pass
+    
+    def test(self, data, labels):
+        ourLabels = self.classify(data)
+        return calculateMisclassificationRate(ourLabels, labels)
+    
+    def classify(self, data):
+        '''input: data to classify, output: array of labels (1/-1)'''
+        pass
+    
 class LogisticRegressionClassifier(LinearClassifier):
     def classify(self, data):
         pass
--- ex4/ex4.py
+++ ex4/ex4.py
@@ -4,21 +4,6 @@ from numpy.core.numeric import count_nonzero
 from scipy.io.matlab.mio import loadmat
 from numpy.lib.shape_base import vsplit
 
-def calculateMisclassificationRate(ours, real):
-    return 1.0 - count_nonzero(ours + real).astype(float64) / ours.size
-
-class LinearClassifier:
-    def train(self, data, labels):
-        '''input: training data and corresponding labels(1,-1), output: nothing'''
-        pass
-    
-    def test(self, data, labels):
-        ourLabels = self.classify(data)
-        return calculateMisclassificationRate(ourLabels, labels)
-    
-    def classify(self, data):
-        '''input: data to classify, output: array of labels (1/-1)'''
-        pass
 
 def doTest(dataSetName, classifier):
     allData = loadmat('data/' + dataSetName + '.mat')
