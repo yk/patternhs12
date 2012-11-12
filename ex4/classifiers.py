d6369c72e68964f0c333033afb401ae90ff34024
yk <yk@ykn>
2012-11-12 16:52:16 +0100

    svm

--- ex4/classifiers.py
+++ ex4/classifiers.py
@@ -1,5 +1,17 @@
+from cvxopt.coneprog import qp
 from numpy import float64
-from numpy.core.numeric import ndarray, count_nonzero
+from numpy.core.fromnumeric import transpose, nonzero
+from numpy.core.numeric import ndarray, count_nonzero, ones, zeros, Inf, \
+    identity, inf, where
+from numpy.lib.shape_base import tile
+from numpy.lib.twodim_base import diag
+from numpy.ma.core import max
+from numpy.ma.extras import dot
+from numpy.matrixlib.defmatrix import mat
+from openopt.oo import QP
+from openopt.solvers.CVXOPT.cvxopt_misc import matrix
+import numpy
+
 def calculateMisclassificationRate(ours, real):
     return 1.0 - count_nonzero(ours + real)*1.0 / ours.size
 
@@ -24,6 +36,32 @@ class DummyClassifier(LinearClassifier):
         
     def train(self, data, labels):
         return 0
+
+class SVMClassifier(LinearClassifier):
+    def __init__(self,C):
+        self.C = C
+        
+    def classify(self, data):
+        t = dot(data,self.w.reshape((-1,1))) + self.w0
+        return t > 0
+    
+    def train(self, data, labels):
+        l = labels.reshape((-1,1))
+        xy = data * l
+        H = dot(xy,transpose(xy))
+        f = -1.0*ones(labels.shape)
+        lb = zeros(labels.shape)
+        ub = self.C * ones(labels.shape)
+        Aeq = labels
+        beq = 0.0
+        p = QP(matrix(H),f.tolist(),lb=lb.tolist(),ub=ub.tolist(),Aeq=Aeq.tolist(),beq=beq)
+        r = p.solve('cvxopt_qp')
+        r.xf[where(r.xf<1e-3)] = 0
+        self.w = dot(r.xf*labels,data)
+        nonzeroindexes = where(r.xf>1e-4)[0]
+        l1 = nonzeroindexes[0]
+        self.w0 = 1.0/labels[l1]-dot(self.w,data[l1])
+        
     
 class LogisticRegressionClassifier(LinearClassifier):
     def classify(self, data):
--- ex4/ex4.py
+++ ex4/ex4.py
@@ -1,9 +1,10 @@
-from classifiers import DummyClassifier
+from classifiers import DummyClassifier, SVMClassifier
 from numpy import float64
 from numpy.core.fromnumeric import transpose
-from numpy.core.numeric import count_nonzero
+from numpy.core.numeric import count_nonzero, arange, ndarray, inf
 from numpy.lib.shape_base import vsplit
 from scipy.io.matlab.mio import loadmat
+import pylab
 
 
 def doTest(dataSetName, classifier):
@@ -21,3 +22,9 @@ if __name__=='__main__':
     myClassifier = DummyClassifier()
     micRate1 = doTest('toy',myClassifier)
     print micRate1
+    svm = SVMClassifier(C=15.0)
+    micRate2 = doTest('toy',svm)
+    print micRate2
+    svm2 = SVMClassifier(C=inf)
+    micRate3 = doTest('zip13',svm2)
+    print micRate3
